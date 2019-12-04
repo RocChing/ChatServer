564683b73a6f7ecb8a1f1feb6d252a2af0c26448
Roc <peng.cheng6@pactera.com>
2019-12-04 10:25:44 +0800

    修改serverConfig

--- ChatServer/ServerConfig.cs
+++ ChatServer/ServerConfig.cs
@@ -6,8 +6,14 @@ namespace ChatServer
 {
     public  class ServerConfig
     {
+        /// <summary>
+        /// 端口号 默认8800
+        /// </summary>
         public int Port { get; set; }
 
+        /// <summary>
+        /// 验证字符串 服务端和客户端必须设置为相同
+        /// </summary>
         public string ValidString { get; set; }
     }
 }
